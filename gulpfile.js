a1c1a820c1772f2eeb593218429f9864b7f7f5e8
Nygren, Pär <par.nygren@sogeti.com>
2018-02-16 10:59:29 +0100

    Added ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var browserify = require('browserify'); //Bundles JS
 var reactify = require('reactify'); //Transforms React JSX to JS
 var source = require('vinyl-source-stream'); //Use conventional text streams with Gulp
 var concat = require('gulp-concat'); //Concatenates files
+var lint = require('gulp-eslint'); //Lint JS files, including JSX
 
 var config = {
     port: 9005,
@@ -60,9 +61,15 @@ gulp.task('css', function() {
         .pipe(gulp.dest(config.paths.dist + '/css'));
 });
 
-gulp.task('watch', function() {
-    gulp.watch(config.paths.html, ['html']);
-    gulp.watch(config.paths.js, ['js']);
+gulp.task('lint', function() {
+    return gulp.src(config.paths.js)
+        .pipe(lint({config: 'eslint.config.json'}))
+        .pipe(lint.format())
 });
 
-gulp.task('default', ['html', 'js', 'css', 'open', 'watch']) ;
+gulp.task('watch', function() {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js', 'lint']);
+});
+
+gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']) ;
