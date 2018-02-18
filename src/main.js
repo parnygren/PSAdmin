03f1d7ca05343d1e94b7a01e6b1388519b226a3e
Nygren, Pär <par.nygren@sogeti.com>
2018-02-18 06:22:46 +0100

    Added simple routing

--- src/main.js
+++ src/main.js
@@ -1,5 +1,31 @@
-$ = jQuery = require('jquery');
+"use strict";
 
-var App = console.log('Hello world from Browserify');
+window.$ = window.jQuery = require('jquery');
+var React = require('react');
+var Home = require('./components/homePage');
+var About = require('./components/about/aboutPage');
 
-module.exports = App;
+var App = React.createClass({
+    render: function () {
+        var Child;
+        switch (this.props.route) {
+            case 'about': Child = About; break;
+            default: Child = Home;
+        }
+
+        return (
+            <div>
+                <Child />
+            </div>
+        );
+    }
+});
+
+function render() {
+    var route = window.location.hash.substr(1);
+    console.log(route);
+    React.render(<App route={route} />, document.getElementById('app'));
+}
+
+window.addEventListener('haschange', render);
+render();
